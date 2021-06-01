4fea742355c1f78ef2ab644e36234a46e0e2bb6a
Andrew <74262186+cevaboyz@users.noreply.github.com>
2021-06-01 15:22:13 +0200

    Create hotjar.js
    first snippet of gtm

---
+++ hotjar.js
@@ -0,0 +1,15 @@
+function() 
+{
+   var hotjarUserID = hj.globals.get("userID").split("-");
+
+      if(hotjarUserId[0] != undefined)
+  {
+	      return hotjarUserid[0];
+  }
+            else
+    {
+	            return0;
+    }
+}
+
+
